4b40a13d232d68de95fb5175b2ca79e4a8a94aa6
Eddie Villanueva <edvillan15@gmail.com>
2023-07-26 00:24:04 +0800

    Create node-install.sh

---
+++ node-install.sh
@@ -0,0 +1,26 @@
+#!/bin/bash 
+
+# Install git, curl -y
+sudo apt install curl git -y
+
+mkdir ~/.kube
+touch ~/.bashrc
+
+# Install Docker
+curl -fsSL https://get.docker.com | bash
+
+# Production
+sudo snap install microk8s --classic
+sudo microk8s status --wait-ready
+sudo microk8s inspect
+sudo microk8s status
+sudo microk8s enable helm3 dns hostpath-storage host-access
+sudo microk8s kubectl config view --raw > $HOME/.kube/config
+
+# Add alias for kubernetes
+echo "alias m='sudo microk8s'" >> ~/.bashrc
+echo "alias k='m kubectl'" >> ~/.bashrc
+echo "alias kubectl='m kubectl'" >> ~/.bashrc
+source ~/.bashrc
+sudo microk8s kubectl get all -A
+sudo swapoff -a
