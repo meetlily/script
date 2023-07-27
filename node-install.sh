48a1c872768c6f18a9a4cffb1c8e48642c64718b
Eddie Villanueva <edvillan15@gmail.com>
2023-07-28 02:52:53 +0800

    Update node-install.sh

--- node-install.sh
+++ node-install.sh
@@ -4,11 +4,12 @@ export DEBIAN_FRONTEND=noninteractive
 # Install git, curl -y
 sudo snap install core --edge
 sudo snap refresh core --edge
-sudo snap mount microk8s
-sudo apt-get update -y && sudo apt-get upgrade -y
-sudo apt install curl git -y
 sudo snap install docker
 sudo snap install microk8s --classic
+
+sudo apt-get update -y && sudo apt-get upgrade -y
+sudo apt install curl git -y
+
 mkdir ~/.kube
 touch ~/.bashrc
 
