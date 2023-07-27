8268b52aed683f1d907937ee2942ef28b213289c
Eddie Villanueva <edvillan15@gmail.com>
2023-07-28 03:29:10 +0800

    Update ubuntu-install.sh

--- ubuntu-install.sh
+++ ubuntu-install.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
 sudo DEBIAN_FRONTEND=noninteractive dpkg --configure -a
+sudo apt-get update -y && sudo apt-get upgrade -y
 # Install git, curl -y
 sudo snap install core --edge
 sudo snap refresh core --edge
 sudo snap install docker
 sudo snap install microk8s --classic
-sudo apt-get update -y && sudo apt-get upgrade -y
+
 sudo apt-get install curl git -y
 # Install NFS server
 
